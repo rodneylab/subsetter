00d306f600ea68b3d03be6cdeb3537ecf92223f4
Rodney Lab <codesign@rodneylab.com>
2025-09-27 15:34:20 +0100

    build(rust-dependencies): 🧱 update crates
    🔧 bump anyhow from 1.0.99 to 1.0.100
    🔧 bump clap from 4.5.44 to 4.5.48
    🔧 bump clap-verbosity-flag from 3.0.3 to 3.0.4
    📦 bump log from 0.4.27 to 0.4.28
    ❗ bump pyo3 from 0.25.1 to 0.26.0
        Updating libc v0.2.175 -> v0.2.176
        Updating memchr v2.7.5 -> v2.7.6
        Updating proc-macro2 v1.0.97 -> v1.0.101
        Updating regex v1.11.1 -> v1.11.3
        Updating regex-automata v0.4.9 -> v0.4.11
        Updating regex-syntax v0.8.5 -> v0.8.6
        Updating serde v1.0.219 -> v1.0.227
          Adding serde_core v1.0.227
        Updating serde_derive v1.0.219 -> v1.0.227
        Updating syn v2.0.104 -> v2.0.106
        Updating target-lexicon v0.13.2 -> v0.13.3
        Updating unicode-ident v1.0.18 -> v1.0.19
        Updating windows-link v0.1.3 -> v0.2.0
        Updating windows-targets v0.53.3 -> v0.53.4

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ use pyo3::{
 };
 
 fn main() -> PyResult<()> {
-    Python::with_gil(|py| {
+    Python::attach(|py| {
         let sys = py.import("sys")?;
         let version: String = sys.getattr("version")?.extract()?;
 
         let locals = [("os", py.import("os")?)].into_py_dict(py)?;
-        let code = c_str!("os.getenv('USER') or os.getenv('USERNAME')or 'Unknown'");
+        let code = c_str!("os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'");
         let user: String = py.eval(code, None, Some(&locals))?.extract()?;
 
         println!("Hello {user}, from Python{version}");
